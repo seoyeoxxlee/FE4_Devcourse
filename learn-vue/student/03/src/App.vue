6010d12a9751c9804f00421906be58a7297aefc3
seoyeoxxlee <seoyeonglee@umail.ucsb.edu>
2025-05-22 15:04:58 +0900

    실습_CountGroup

--- learn-vue/student/03/src/App.vue
+++ learn-vue/student/03/src/App.vue
@@ -1,14 +1,10 @@
 <script>
-// import CountButtons from './components/CountButtons.vue';
-import CountButtonsWithProp from './components/CountButtonsWithProp.vue';
-import CountDisplay from './components/CountDisplay.vue';
+import CountGroup from './components/CountGroup.vue';
 
 export default {
   name: 'App',
   components: {
-    CountDisplay,
-    // CountButtons,
-    CountButtonsWithProp,
+    CountGroup,
   },
   data() {
     return {
@@ -30,9 +26,12 @@ export default {
 </script>
   
 <template>
-  <CountDisplay :count="count"/>
-  <!-- <CountButtons @increment="increment" @reset="reset" @decrement="decrement"/> -->
-  <CountButtonsWithProp :increment="increment" :reset="reset" :decrement="decrement"/>
+  <CountGroup 
+    :count="count"
+    @increment="increment" 
+    @reset="reset" 
+    @decrement="decrement"
+  />
 </template>
   
 <style scoped>
---
+++ learn-vue/student/03/src/components/CountGroup.vue
@@ -0,0 +1,30 @@
+<script>
+import CountButtons from './CountButtons.vue';
+import CountDisplay from './CountDisplay.vue';
+
+export default {
+  name: 'CountGroup',
+  components: {
+    CountDisplay,
+    CountButtons,
+  },
+  props: {
+    count: {
+        type: Number,
+    },
+  },
+  emits: ['increment', 'reset', 'decrement'],
+}
+</script>
+  
+<template>
+    <CountDisplay :count="count"/>
+    <CountButtons 
+        @increment="$emit('increment')"
+        @reset="$emit('reset')"
+        @decrement="$emit('decrement')"
+    />
+</template>
+  
+<style scoped>
+</style>
