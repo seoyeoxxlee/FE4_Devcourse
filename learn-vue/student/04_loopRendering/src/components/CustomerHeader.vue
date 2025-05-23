cb74c219d53791b216761753e887234d77f05ba8
seoyeoxxlee <seoyeonglee@umail.ucsb.edu>
2025-05-23 17:15:23 +0900

    [실습] 반복문 - 카드렌더링

---
+++ learn-vue/student/04_loopRendering/src/components/CustomerHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'CustomerHeader',
+  props: {
+    showAll: {
+      type: Boolean,
+    },
+  },
+  emits: ['viewAll'],
+}
+</script>
+<template>
+  <div class="flex items-center justify-between mb-4">
+    <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Customers</h5>
+    <button
+      v-if="!showAll"
+      class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+      @click="$emit('viewAll')"
+    >
+      View all
+    </button>
+  </div>
+</template>
+<style scoped></style>
---
+++ learn-vue/student/04_loopRendering/src/components/CustomerList.vue
@@ -0,0 +1,38 @@
+<script>
+import CustomerListItem from './CustomerListItem.vue'
+
+export default {
+  name: 'CustomerList',
+  components: {
+    CustomerListItem,
+  },
+  props: {
+    customers: {
+      type: Array,
+      required: true,
+    },
+    showAll: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    displayedCustomer() {
+      return this.showAll ? this.customers : this.customers.slice(0, 5)
+    },
+  },
+}
+</script>
+<template>
+  <div class="flow-root">
+    <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+      <li
+        is="vue:customer-list-item"
+        v-for="customer in displayedCustomer"
+        :key="customer.id"
+        :customer="customer"
+      />
+    </ul>
+  </div>
+</template>
+<style scoped></style>
